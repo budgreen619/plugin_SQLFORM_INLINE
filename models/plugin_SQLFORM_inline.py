b8834526dd43bcb0662d0297f7a37e4d795d3dc9
pjryan126 <pjryan126@gmail.com>
2013-12-18 18:52:23 -0500

    initial files

---
+++ models/plugin_SQLFORM_inline.py
@@ -0,0 +1,5 @@
+# coding: utf8
+
+from gluon.custom_import import track_changes; track_changes(True)
+
+response.files.append(URL(r=request, c='static', f='plugin_SQLFORM_INLINE/css/plugin_SQLFORM_INLINE.css'))
